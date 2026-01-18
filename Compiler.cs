9712bff04d830c1aeabebfa0dfd66b6574c70eea
wesbanq <217015445+wesbanq@users.noreply.github.com>
2026-01-18 14:40:29 +0200

    Started on the lexer

--- Compiler.cs
+++ Compiler.cs
@@ -4,16 +4,85 @@ using System.Text;
 
 namespace stilt
 {
-	public class Compiler
+	public class FileRange
 	{
-		public static List<Token> Tokenize()
-		{
+		public int Start;
+		public int End;
+		public int Length => End - Start;
+		public string Filename;
 
+		public FileRange(int start, int end, string filename)
+		{
+			Start = start;
+			End = end;
+			Filename = filename;
 		}
 
+		public bool Overlaps(FileRange other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (!string.Equals(Filename, other.Filename, StringComparison.Ordinal))
+			{
+				return false;
+			}
+
+			return Start < other.End && End > other.Start;
+		}
+
+		public static List<Token> RemoveOverlaps(List<Token> priorityRanges, List<Token> ranges)
+		{
+			//precedence - longest > shortest : symbol > regex
+			//assume both ranges are sorted
+			var finalList = new List<Token>();
+			foreach (Token token in priorityRanges)
+			{
+				//Token tokenOvelapped = null;
+				foreach (Token otherToken in ranges)
+				{
+					if (!token.Range.Overlaps(otherToken.Range))
+					{
+						//tokenOvelapped = otherToken;
+						//break;
+						finalList.Add(otherToken);
+					}
+				}
+
+				List<Token> overlappingTokens = priorityRanges
+					.Where(t => t.Range.Overlaps(token.Range) && !ReferenceEquals(token, t))
+					.ToList();
+				if (overlappingTokens.Count > 0)
+				{
+					Token max = overlappingTokens.MaxBy(t => t.Range.Length);
+					if (max.Range.Length == token.Range.Length)
+					{
+						Program.Dump(max);
+						Program.Dump(token);
+						throw new Lexer.OverlappingTokensException("Overlapping tokens", token, max);
+					}
+					else
+					{
+						finalList.Add(max.Range.Length > token.Range.Length ? max : token);
+					}
+				}
+				else
+				{
+					finalList.Add(token);
+				}
+			}
+			return finalList;
+		}
+	}
+
+	public abstract class Compiler
+	{
 		public static void Build(ProgramArgs args)
 		{
 			Console.WriteLine(args.MainCodeFilepath);
+
 		}
 	}
 }
---
+++ Lexer.cs
@@ -0,0 +1,84 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace stilt
+{
+	public abstract class Lexer
+	{
+		static void GetStringAttribute(out List<string[]> symbols, out List<string[]> regex)
+		{
+			symbols = new List<string[]>();
+			regex = new List<string[]>();
+
+			foreach (var name in typeof(Token.Tokens).GetEnumNames())
+			{
+				symbols.Add(typeof(Token.Tokens).GetField(name).GetCustomAttributes<SymbolAttribute>()?.Where(a => !a.IsRegex).Select(a => Regex.Escape(a.Symbol)).ToArray());
+				regex.Add(typeof(Token.Tokens).GetField(name).GetCustomAttributes<SymbolAttribute>()?.Where(a => a.IsRegex).Select(a => a.Symbol).ToArray());
+			}
+		}
+
+		public class OverlappingTokensException : Exception
+		{
+			Token Token1 { get; set; }
+			Token Token2 { get; set; }
+
+
+			public OverlappingTokensException(string message, Token token1, Token token2) : base(message)
+			{
+				Token1 = token1;
+				Token2 = token2;
+			}
+		}
+
+		static string StripComments(string code)
+		{
+			const string commentRegex1 = """#.*""";
+			const string commentRegex2 = """##(?:.*\s)*##""";
+			const string commentRegex3 = """\n\n+""";
+			return Regex.Replace(Regex.Replace(Regex.Replace(code, commentRegex2, ""), commentRegex1, ""), commentRegex3, "\n");
+		}
+
+		static List<Token> GetTokenMatches(string code, List<string[]> rules, string filepath)
+		{
+			var tokens = new List<Token>();
+
+			for (int i = 0; i < rules.Count; ++i)
+			{
+				if (rules[i] == null || rules[i].Length == 0) continue;
+				foreach (var rule in rules[i])
+				{
+					var matchCollection = Regex.Matches(code, rule);
+					foreach (Match match in matchCollection)
+					{
+						var newToken = new Token();
+
+						newToken.Range = new FileRange(match.Index, match.Index + match.Length, filepath);
+						newToken.Which = (Token.Tokens)i;
+
+						Program.Dump(newToken);
+						tokens.Add(newToken);
+					}
+				}
+			}
+
+			return tokens;
+		}
+
+		public static List<Token> Tokenize(string filepath)
+		{
+			var code = StripComments(File.ReadAllText(filepath));
+			var tokens = new List<Token>();
+
+			GetStringAttribute(out var symbols, out var regex);
+			tokens = FileRange.RemoveOverlaps(
+				GetTokenMatches(code, symbols, filepath).OrderBy(t => t.Range.Start).ThenBy(t => t.Range.End).ToList(),
+				GetTokenMatches(code, regex, filepath).OrderBy(t => t.Range.Start).ThenBy(t => t.Range.End).ToList()
+			);
+			//tokens = GetTokenMatches(code, regex, filepath).OrderBy(t => t.Range.Start).ThenBy(t => t.Range.End).ToList();
+
+			tokens.ForEach(t => Program.Dump(t));
+
+			return tokens;
+		}
+	}
+}
